5340f94b947cb1c19e5cb3f1f9aa5ac8a6b15e79
kapehh <mail@kapehh.net>
2024-10-06 03:27:24 +0300

    test

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="row">
-        <div class="col-md-3" style="border: 1px solid green;">
-            SIDEBAR
+    <div class="row" style="height: 100vh;">
+        <div class="col-md-3" style="background-color: indigo;">
+            <div class="p-3 m-0 border-0">
+                <div class="d-grid gap-2">
+                    <button class="btn btn-outline-light btn-sm text-start" type="button">Issues</button>
+                    <button class="btn btn-outline-light btn-sm text-start" type="button">Projects</button>
+                </div>
+            </div>
         </div>
-        <div class="col-md-9" style="border: 1px solid blue;">
+        <div class="col-md-9">
             <slot></slot>
         </div>
     </div>
