4b9241cfc566b9b4ce696dce4b8c1a0c3867215a
dv4mp1r3 <somebody@somewhere>
2025-05-06 22:55:07 +0300

    21: +detail page components for teams, projects and users and +read entity service functions

---
+++ src/service/readEntity.ts
@@ -0,0 +1,13 @@
+import {EntityName} from "../models/count.ts";
+import {sendGet} from "../utils/requests.ts";
+
+export function getEntityId(e: EntityName): number {
+    return Number.parseInt(location.pathname.replace(`/${e}/`, ''));
+}
+
+export async function readEntity(e: EntityName, id: number) : Promise<any> {
+    const response = await sendGet(`/${e}/${id}`);
+    if (response.status == 200) {
+        return await response.json();
+    }
+}
