030cc8c0944e2075ba268992226c49b6270a7ea2
dv4mp1r3 <somebody@somewhere>
2025-06-26 21:56:24 +0300

    20: +update org, fix type in proj

--- src/pages/organizations/read.vue
+++ src/pages/organizations/read.vue
@@ -6,9 +6,13 @@ import {ref} from "vue";
 import {Organization} from "../../models/organization.ts";
 import {EntityName} from "../../models/count.ts";
 import {getEntityId, readEntity} from "../../service/readEntity.ts";
+import {updateOrganization} from "../../service/updateEntity.ts";
+import Button from "../../components/Button.vue";
 
 const loaded = ref(false);
-const org = ref<Organization | undefined>(undefined);
+const org = ref<Organization>({} as Organization);
+let isEditing = ref<boolean>(false);
+const loading = ref<boolean>(false);
 
 async function initLoad() {
   try {
@@ -17,6 +21,25 @@ async function initLoad() {
   } catch (e) {
     console.log(e);
   }
+}
+
+function editSwitch() : void {
+  isEditing.value = !isEditing.value;
+}
+
+function getButtonCaption(): string {
+  return isEditing.value ? 'SAVE' : 'EDIT';
+}
+
+async function actionButtonClick(): Promise<void> {
+  if (isEditing.value) {
+    console.log('save action');
+    await updateOrganization(loading, org.value);
+    console.log('edit disabled');
+  } else {
+    console.log('edit enabled');
+  }
+  editSwitch();
 }
 
 initLoad();
@@ -29,6 +52,13 @@ initLoad();
       <h2>
         {{ org?.Name}}
       </h2>
+      <div>
+        <Button @click="actionButtonClick">{{getButtonCaption()}}</Button>
+      </div>
+      <div>
+        <p v-if="!isEditing">{{org?.Name}}</p>
+        <input type="text" v-if="isEditing" v-model="org.Name" />
+      </div>
     </template>
 
   </Sidebar>
