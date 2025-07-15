28675bed9247f572a6103fc91e69bf1db8180292
dv4mp1r3 <somebody@somewhere>
2025-07-15 20:49:37 +0300

    26: show labels on removable chip

--- src/pages/users/create.vue
+++ src/pages/users/create.vue
@@ -8,14 +8,14 @@ import {createUser} from "../../service/createEntity.ts";
 import {UserCreate} from "../../models/user.ts";
 import {loadOrganizations, loadTeams} from "../../service/loadList.ts";
 import {SelectBoxOption} from "../../models/SelectBoxOption.ts";
-import SelectBox from "../../components/SelectBox.vue";
+import SelectBox, {SelectBoxElement} from "../../components/SelectBox.vue";
 import RemovableChip from "../../components/RemovableChip.vue";
 
 const loading = ref<boolean>(false)
 const orgOffset = ref(0);
 const teamOffset = ref(0);
-const teamId = ref(0);
-const orgId = ref(0);
+const teamId = ref('');
+const orgId = ref('');
 const user = ref<UserCreate>({
   Username: '',
   Email: '',
@@ -23,6 +23,8 @@ const user = ref<UserCreate>({
   TeamIds: [],
   OrganizationIds: [],
 });
+const selectedTeams = ref<Array<SelectBoxElement>>([]);
+const selectedOrgs = ref<Array<SelectBoxElement>>([]);
 
 const optionsTeams = ref(Array<SelectBoxOption>());
 const optionsOrgs = ref(Array<SelectBoxOption>());
@@ -46,8 +48,12 @@ async function loadLists() {
   loading.value = false;
 }
 
-function addIdOnlyIfNew(id: number | undefined, data: Array<number>) : void {
-  if (id === undefined || id <= 0) {
+function addIdOnlyIfNew(el: SelectBoxElement, data: Array<number>, selectedData: Array<SelectBoxElement>) : void {
+  if (el.value === undefined) {
+    return;
+  }
+  const id = Number.parseInt(el.value);
+  if (id <= 0) {
     return;
   }
   for (let i of data) {
@@ -56,27 +62,36 @@ function addIdOnlyIfNew(id: number | undefined, data: Array<number>) : void {
     }
   }
   data.push(id);
+  selectedData.push(el);
 }
 
 function removeFromArrByValue(data: Array<number>, value: number) : Array<number> {
   return data.filter(el => el !== value);
 }
 
-function onOrgChanged(id: number|undefined) : void {
-  addIdOnlyIfNew(id, user.value.OrganizationIds);
-  orgId.value = 0;
+function onOrgChanged(el: SelectBoxElement) : void {
+  if (el.value === undefined) {
+    return;
+  }
+  addIdOnlyIfNew(el, user.value.OrganizationIds, selectedOrgs.value);
+  orgId.value = '';
 }
 
-function onTeamChanged(id: number|undefined) : void {
-  addIdOnlyIfNew(id, user.value.TeamIds);
-  teamId.value = 0;
+function onTeamChanged(el: SelectBoxElement) : void {
+  if (el.value === undefined) {
+    return;
+  }
+  addIdOnlyIfNew(el, user.value.TeamIds, selectedTeams.value);
+  teamId.value = '';
 }
 
 function removeOrg(value: string) : void {
+  selectedOrgs.value = selectedOrgs.value.filter(el => el.value !== value);
   user.value.OrganizationIds = removeFromArrByValue(user.value.OrganizationIds, Number.parseInt(value));
 }
 
 function removeTeam(value: string) : void {
+  selectedTeams.value = selectedTeams.value.filter(el => el.value !== value);
   user.value.TeamIds = removeFromArrByValue(user.value.TeamIds, Number.parseInt(value));
 }
 
@@ -86,21 +101,21 @@ loadLists();
 <template>
     <Sidebar :active=MenuItem.Users>
         <h1>Create new User</h1>
-        <div v-if="user.OrganizationIds.length > 0" class="organizations">
+        <div v-if="selectedOrgs.length > 0" class="organizations">
           <label>Organizations:</label>
           <div class="chip-group org-chip-group">
-            <removable-chip v-for="org in user.OrganizationIds"
-                :text="org.toString()"
-                :payload="org.toString()"
+            <removable-chip v-for="org in selectedOrgs"
+                :text="org.name"
+                :payload="org.value"
                 :chip-class="`chip-el`" @onClose="removeOrg" />
           </div>
         </div>
-        <div v-if="user.TeamIds.length > 0" class="teams">
+        <div v-if="selectedTeams.length > 0" class="teams">
           <label>Teams:</label>
           <div class="chip-group team-chip-group">
-            <removable-chip v-for="team in user.TeamIds"
-                :text="team.toString()"
-                :payload="team.toString()"
+            <removable-chip v-for="team in selectedTeams"
+                :text="team.name"
+                :payload="team.value"
                 :chip-class="`chip-el`" @onClose="removeTeam" />
           </div>
         </div>
