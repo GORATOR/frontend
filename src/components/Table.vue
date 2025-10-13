441ea3771847462edddd772eaf97cac0c129f2d8
dv4mp1r3 <somebody@somewhere>
2025-10-13 23:08:16 +0300

    7: added sort by count for issues table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,66 +1,81 @@
 <script setup lang="ts">
 const props = defineProps<{
-    headers: string[],
-    rows: any[]
+  headers: string[],
+  rows: any[],
+  sortable?: boolean
 }>()
+
+const emit = defineEmits<{
+  (e: 'sort', header: string): void
+}>()
+
+function handleHeaderClick(header: string) {
+  if (props.sortable) {
+    emit('sort', header)
+  }
+}
 </script>
 
 <template>
-    <div class="table">
-        <div class="row header">
-            <div class="cell" v-for="header in props.headers">{{ header }}</div>
-        </div>
-        <div v-for="item in props.rows" class="row data">
-            <div class="cell" v-for="header in props.headers">
-              <a v-if="item.url !== undefined" :href="item.url">{{ item[header] }}</a>
-              <div v-if="item.url === undefined">{{ item[header] }}</div>
-            </div>
-        </div>
+  <div class="table">
+    <div class="row header">
+      <div
+          class="cell"
+          v-for="header in props.headers"
+          :class="{ sortable: props.sortable }"
+          @click="handleHeaderClick(header)"
+      >
+        {{ header }}
+      </div>
     </div>
+    <div v-for="item in props.rows" class="row data">
+      <div class="cell" v-for="header in props.headers">
+        <a v-if="item.url !== undefined" :href="item.url">{{ item[header] }}</a>
+        <div v-if="item.url === undefined">{{ item[header] }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/variables' as *;
 
 .table {
-    display: table;
-    width: 100%;
-    border: 1px solid $main_theme_border_color;
-    border-radius: 5px;
-    overflow: hidden;
+  display: table;
+  width: 100%;
+  border: 1px solid $main_theme_border_color;
+  border-radius: 5px;
+  overflow: hidden;
 
-    .row {
-        display: table-row;
+  .row {
+    display: table-row;
 
-        // &:nth-child(2n+1) {
-        //     background-color: red;
-        // }
+    &.header {
+      color: $main_theme_border_color_darker1;
+      background-color: $main_theme_border_color_lighter1;
 
-        &.header {
-            color: $main_theme_border_color_darker1;
-            background-color: $main_theme_border_color_lighter1;
+      .cell {
+        display: table-cell;
+        border-top: none;
+        padding: 15px 10px;
 
-            .cell {
-                display: table-cell;
-                border-top: none;
-                padding: 15px 10px;
-            }
+        &.sortable {
+          cursor: pointer;
+          user-select: none;
+
+          &:hover {
+            background-color: $main_theme_border_color;
+          }
         }
-
-        &.data .cell {
-            display: table-cell;
-            border-top: 1px solid $main_theme_border_color;
-            padding: 10px;
-            font-weight: normal;
-
-            // &:first-child {
-            //     width: 20px;
-            // }
-
-            // &.index {
-            //     color: $main_theme_border_color_darker1;
-            // }
-        }
+      }
     }
+
+    &.data .cell {
+      display: table-cell;
+      border-top: 1px solid $main_theme_border_color;
+      padding: 10px;
+      font-weight: normal;
+    }
+  }
 }
 </style>
--- src/pages/issues/list.vue
+++ src/pages/issues/list.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
 import Sidebar from '../../components/Sidebar.vue'
 import Paging from '../../components/paging/Paging.vue'
+import Issue from '../../components/issue/Issue.vue'
 import {EntityCount, EntityName} from '../../models/count.ts'
 import {PageSelectEvent} from '../../models/pagingPageSelect.ts'
-import Issue from '../../components/issue/Issue.vue'
 import IssueChart from '../../components/issue/IssueChart.vue'
 import {MenuItem} from '../../models/sidebarMenuItem.ts'
 import {loadIssuesAggregated, AggregatedIssue} from "../../service/loadList.ts";
@@ -16,16 +16,60 @@ const loaded = ref(false)
 const count = ref<EntityCount>({ count: 0, entity: EntityName.Envelope })
 const page = ref(1)
 const offset = ref(0)
+const sortBy = ref<string>('count')
+const sortOrder = ref<string>('DESC')
+
+const sortLabel = computed(() => {
+    if (sortBy.value === 'count') {
+        return sortOrder.value === 'DESC' ? 'Count ↓' : 'Count ↑';
+    }
+    return 'Count';
+})
 
 async function pageSelect(e: PageSelectEvent) {
     page.value = e.page
     offset.value = e.offset
-    list.value = await loadIssuesAggregated(loaded, offset.value);
+    list.value = await loadIssuesAggregated(
+        loaded,
+        offset.value,
+        10,
+        sortBy.value === 'first_id' ? undefined : sortBy.value,
+        sortBy.value === 'first_id' ? undefined : sortOrder.value
+    );
+}
+
+async function toggleSort() {
+    // Cycle through: count DESC -> count ASC -> first_id DESC (no sort)
+    if (sortBy.value === 'first_id') {
+        sortBy.value = 'count';
+        sortOrder.value = 'DESC';
+    } else if (sortBy.value === 'count' && sortOrder.value === 'DESC') {
+        sortOrder.value = 'ASC';
+    } else {
+        sortBy.value = 'first_id';
+        sortOrder.value = 'DESC';
+    }
+
+    offset.value = 0;
+    page.value = 1;
+    list.value = await loadIssuesAggregated(
+        loaded,
+        offset.value,
+        10,
+        sortBy.value === 'first_id' ? undefined : sortBy.value,
+        sortBy.value === 'first_id' ? undefined : sortOrder.value
+    );
 }
 
 async function initLoad() {
     count.value = await loadAggregatedIssuesCount();
-    list.value = await loadIssuesAggregated(loaded, offset.value);
+    list.value = await loadIssuesAggregated(
+        loaded,
+        offset.value,
+        10,
+        sortBy.value,
+        sortOrder.value
+    );
 }
 
 initLoad()
@@ -36,11 +80,21 @@ initLoad()
         <template v-if="loaded">
             <h2>Issues</h2>
 
-            <IssueChart :days="14" />
+          <IssueChart :days="14"/>
 
-            <div class="issue-container">
-                <div v-for="item in list" class="issue">
-                    <Issue :envelope="item.envelope" :count="item.count" />
+            <div class="issues-table">
+                <div class="table-header">
+                    <div class="header-cell issue-type">Exception Type</div>
+                    <div class="header-cell issue-value">Exception Value</div>
+                    <div class="header-cell issue-chart">Chart (7 days)</div>
+                    <div class="header-cell issue-count sortable" @click="toggleSort">
+                        {{ sortLabel }}
+                    </div>
+                </div>
+                <div class="issue-container">
+                    <div v-for="item in list" class="issue">
+                        <Issue :envelope="item.envelope" :count="item.count" />
+                    </div>
                 </div>
             </div>
 
@@ -56,12 +110,57 @@ initLoad()
     padding-top: 14px;
 }
 
-.issue {
+.issues-table {
     border: 1px solid $main_theme_border_color;
-    border-top: none;
+    border-radius: 5px;
+    overflow: hidden;
+}
+
+.table-header {
+    display: flex;
+    background-color: $main_theme_border_color_lighter1;
+    color: $main_theme_border_color_darker1;
+    padding: 15px 10px;
+    font-weight: bold;
+    border-bottom: 1px solid $main_theme_border_color;
+}
+
+.header-cell {
+    padding: 0 5px;
+
+    &.issue-type {
+        flex: 0 0 150px;
+    }
+
+    &.issue-value {
+        flex: 1;
+        min-width: 0;
+    }
+
+    &.issue-chart {
+        flex: 0 0 120px;
+    }
+
+    &.issue-count {
+        flex: 0 0 100px;
+        text-align: right;
+    }
+
+    &.sortable {
+        cursor: pointer;
+        user-select: none;
+
+        &:hover {
+            color: rgb(0, 106, 255);
+        }
+    }
+}
+
+.issue {
+    border-top: 1px solid $main_theme_border_color;
 
     &:first-child {
-        border-top: 1px solid $main_theme_border_color;
+        border-top: none;
     }
 }
 </style>
